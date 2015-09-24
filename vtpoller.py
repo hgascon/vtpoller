cc422aa37ab13b39dca317c74f51be987adccde0
Hugo Gascon <hgascon@mail.de>
2015-09-24 21:15:51 +0200

    initial commit

---
+++ vtpoller.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+
+"""
+Query VT for samples submitted today and in previous days
+with less than 3 detections and save their detections.
+
+DATA FILE:
+query date, submission date, hash, detections
+
+"""
+
+
+class VTPoller:
+
+    def __init__():
+        return
+
+    def query_today_samples(self):
+        return
+
+    def query_past_samples(self):
+        return
