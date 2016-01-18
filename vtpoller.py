dda4a120eca2f62073c1e4467b18c09340a3607d
Hugo Gascon <hgascon@mail.de>
2016-01-18 23:58:23 +0100

    First seen entry udpates in data frame

--- vtpoller.py
+++ vtpoller.py
@@ -1,11 +1,13 @@
 __author__ = 'hgascon'
 
 import os
+import sys
 import urllib
 import urllib2
 import json
 import time
-import apikey
+import requests
+from apikey import API_KEY
 from datetime import date, timedelta
 from shutil import copyfile
 from pandas import DataFrame
@@ -20,12 +22,72 @@ query date, submission date, hash, detections
 """
 
 DATAFILE = "malware_metadata.csv"
-COLUMNS = ["query_time", "scan_time", "hash", "detections"]
+FILE_TYPE = 'pe'
+FILE_MAX_SIZE = '200kb'
+COLUMNS = ["query_time", "scan_date", "hash", "detections"]
 
 
-#TODO add android tag to query
-def query(parameters):
-    url = "https://www.virustotal.com/vtapi/v2/file/report"
+def run():
+    datafile_path = os.path.abspath(DATAFILE)
+
+    # create data file if it doesn't exist
+    if not os.path.exists(datafile_path):
+        print "[*] Creating data frame..."
+        df = DataFrame(columns=COLUMNS)
+        df.to_csv(datafile_path)
+
+    # backup data file
+    print "[*] Backing data file up..."
+    copyfile(datafile_path, "{}.bak".format(datafile_path))
+
+    # load dataframe and find hashes with max 3 detections
+    df = pd.read_csv(datafile_path)
+    undetected_hashes = ', '.join(df.hash[df.detections <= 3].values)
+
+    # add results from existing samples
+    parameters = {"apikey": API_KEY,
+                  "resource": undetected_hashes}
+    if undetected_hashes:
+        print "[*] Retrieving reports for undetected samples..."
+        jsonr = retrieve_reports(parameters)
+        df = add_results(jsonr, df)
+
+    # retrieve hashes from new samples under criteria
+    first_seen_plus = (date.today() - timedelta(1)).strftime('%Y-%m-%d')
+    first_seen_minus = date.today().strftime('%Y-%m-%d')
+    query = "fs:{}+ fs:{}- tag:{} size:{}- positives:0".format(
+            first_seen_plus, first_seen_minus, FILE_TYPE, FILE_MAX_SIZE)
+    parameters = {"apikey": API_KEY,
+                  "query": query}
+    print "[*] Retrieving new samples..."
+    response_new_hashes = retrieve_hashes(parameters)
+
+    # query for reports of new hashes
+    try:
+        new_hashes = ', '.join(response_new_hashes['hashes'])
+        parameters = {"apikey": API_KEY,
+                      "resource": new_hashes}
+    except:
+        "No new new hashes found!"
+        new_hashes = ''
+
+    if new_hashes:
+        jsonr = retrieve_reports(parameters)
+        df = add_results(jsonr, df)
+
+    # update results
+    print "[*] Saving data file..."
+    df.to_csv(datafile_path, index=False)
+
+
+def retrieve_hashes(parameters):
+    url = 'https://www.virustotal.com/intelligence/search/programmatic'
+    response = requests.get(url, params=parameters)
+    return response.json()
+
+
+def retrieve_reports(parameters):
+    url = 'https://www.virustotal.com/vtapi/v2/file/report'
     data = urllib.urlencode(parameters)
     req = urllib2.Request(url, data)
     response = urllib2.urlopen(req)
@@ -46,37 +108,8 @@ def add_results(jsonr, df):
         results.append(new_row)
 
     results = DataFrame(results, columns=COLUMNS)
-    df.append(results)
+    df = df.append(results)
     return df
 
-
-datafile_path = os.path.abspath(DATAFILE)
-
-# create data file if it doesn't exist
-if not os.path.exists(datafile_path):
-    df = DataFrame(columns=COLUMNS)
-    df.to_csv(datafile_path)
-
-# backup data file
-copyfile(datafile_path, "{}.bak".format(datafile_path))
-
-# load dataframe and find hashes with max 3 detections
-df = pd.read_csv(datafile_path)
-undetected_hashes = ','.join(df.hash[df.detections <= 3].values)
-
-# add results from existing samples
-parameters = {"resource": undetected_hashes,
-              "apikey": apikey.APIKEY}
-jsonr = query(parameters)
-df = add_results(jsonr, df)
-
-# add results from new samples
-scan_date = (date.today() - timedelta(1)).strftime('%Y-%m-%d')
-parameters = {"resource": "",
-              "apikey": apikey.APIKEY,
-              "scan_date": scan_date}
-jsonr = query(parameters)
-df = add_results(jsonr, df)
-
-# update results
-df.to_csv(df, index=False)
+if __name__ == "__main__":
+    run()
